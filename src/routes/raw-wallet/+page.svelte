7d4749ebf62edd77f016610d75b84e4bf8df0573
Your Name <yt.atchyut@gmail.com>
2024-07-10 19:03:26 +0530

    first commit

---
+++ src/routes/raw-wallet/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { PrivateKey } from '@bsv/sdk'
+
+    let WIF:string
+    let address:string
+
+    function createKeys () {
+
+        const privKey = PrivateKey.fromRandom()
+        WIF = privKey.toWif()
+        console.log("Private Key WIF formate is ", WIF)
+        address = privKey.toAddress()
+        console.log("Your bitcoin address is ", address)
+    }
+
+
+</script>
+
+<section>
+    <button on:click={createKeys} class="bg-yellow-500 hover:bg-blue-700 text-white font-bold mt-5 ml-2 px-2 rounded " >Generate New Wallet Keys</button>
+    <p>Private Key WIF formate is {WIF}</p>
+    <p>Your Bitcoin Address is {address}</p>
+</section>
